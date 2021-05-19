4bf9fb2b929d57fa646edb5a03ee6e908e3fc314
asefn <asef.novruzlu@gmail.com>
2021-05-19 23:50:48 +0200

    commit

--- app/src/main/java/com/example/covid19/notification/APIService.java
+++ app/src/main/java/com/example/covid19/notification/APIService.java
@@ -8,7 +8,7 @@ import retrofit2.http.POST;
 public interface APIService {
     @Headers({
             "Content-Type:application/json",
-            "Authorization:key=AAAAo5BXs64:APA91bEKnZW8qjYksbe3OsY7gAcxyG4a_tQsGcaD62YSNu4Vk9DjO6_hXk_vTeCz8gfiQ3IdeVNCDdyf3o8WdQlFpJFcFL8Ha26XCTKupgv_71MuVt9RU7S1c9byKRRAwYu61iQ20GK_"
+            "Authorization:key=key should be here"
 
     })
     @POST("fcm/send")
